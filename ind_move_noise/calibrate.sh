fd2b7a23502b7c75d9752d6e7b85695e2eda661c
TomShkurti <tes77@case.edu>
2022-04-02 15:47:09 -0400

    Add random noise in movement.

---
+++ ind_move_noise/calibrate.sh
@@ -0,0 +1,67 @@
+roscore &
+sleep 3
+
+rosparam set /rail_cal_service/camera_inaccurate/min_area 10
+rosparam set /rail_cal_service/camera_inaccurate/max_area 80000
+#We don't want the solver to start with EXACTLY the right answer.
+rostopic pub /camera_inaccurate/camera_info sensor_msgs/CameraInfo "header:
+  seq: 0
+  stamp: {secs: 0, nsecs: 0}
+  frame_id: ''
+height: 480
+width: 640
+distortion_model: 'plumb_bob'
+D: [-0.15, 0.3, -0.001, 0.005, -0.03]
+K: [500.0, 0.0, 275.0, 0.0, 500.0, 200.0, 0.0, 0.0, 1.0]
+R: [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+P: [500.0, 0.0, 275.0, 0.0, 0.0, 500.0, 200.0, 0.0, 0.0, 0.0, 1.0, 0.0]
+binning_x: 0
+binning_y: 0
+roi: {x_offset: 0, y_offset: 0, height: 0, width: 0, do_rectify: false}" &
+sleep 3
+
+rosrun image_view image_view image:=/camera/image_raw __name:=view_1 &
+rosrun image_view image_view image:=/camera_inaccurate/observer_results_image __name:=view_2 &
+sleep 3
+
+for s in $( seq 0.000 0.0005 0.01 )
+do
+	RANDOM=12345
+
+	list=$(find $PWD/data_$s -type f | shuf)
+	sorted_list=$(find $PWD/data_$s -type f | sort)
+	n=200
+	for f in $list
+	do
+		if ! (($n % 10)) && (($n > 0)) #Calibrate every ten files blanked.
+		then
+			echo "s = $s, n = $n"
+			rosrun intrinsic_cal rail_ical _target_rows:=6 _target_cols:=8 _target_circle_dia:=0.03 _target_spacing:=0.0502 _target_to_rail_distance:=0.35 _num_camera_locations:=188 _camera_spacing:=0.005319149 _image_topic:=/camera/image_raw _camera_name:=camera_inaccurate _image_height:=480 _image_width:=640 _use_circle_detector:=true &
+			cal_pid=$!
+			sleep 3
+		
+			rosservice call --wait /RailCalService "allowable_cost_per_observation: 9000.0" &
+			srv_pid=$!
+			sleep 1
+			for i in $sorted_list
+			do
+				rosrun image_publisher image_publisher $i __name:=camera &
+				rosparam set /rail_cal_service/camera_ready true
+				while $(rosparam get /rail_cal_service/camera_ready) && ps -p $srv_pid > /dev/null
+				do
+					: #echo "Waiting"
+				done
+				killall image_publisher
+			done
+		
+			echo "WAITING ON SERV TO FINISH"
+			wait $srv_pid
+			killall rail_ical
+			wait $cal_pid
+		fi
+		#Blank one file to reduce the total number
+		n=$(($n-1))
+		cp $PWD/../raw_data_ind/flatblack.png $f
+	done
+
+done
---
+++ ind_move_noise/generate.sh
@@ -0,0 +1,53 @@
+for s in $( seq 0.000 0.0005 0.01 )
+do
+	mkdir $PWD/data_$s
+	roslaunch intrinsic_simulation cam_launch.launch acquire_file:=$PWD/../raw_data_cwru/task_description.yml cam_file:=$PWD/../raw_data_ind/camera_headon.yml &
+	sleep 30
+	rosrun image_view image_saver image:=/camera/image_raw_color _save_all_image:=false __name:=saver _filename_format:=$PWD/img_tmp.png &
+	sleep 30
+	
+	echo "s = $i"
+	RANDOM=12345
+	for z in $( seq 0.35 0.005 1.35)
+	do
+		u_one=$( echo "$RANDOM / 32767" | genius)
+		u_two=$( echo "$RANDOM / 32767" | genius)
+		normal=$(echo "sqrt(-2 * ln($u_one)) * cos(2 * pi * $u_two)" | genius) #https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
+		z_wobble=$(echo "($normal * -0.5 * $s) + $z" | genius)
+		
+		echo "X is -0.1757 Y is -0.1288 Z is $z"
+		rosservice call /gazebo/set_model_state "model_state:
+  model_name: 'target'
+  pose:
+    position:
+      x: -0.1757
+      y: -0.1288
+      z: $z_wobble
+    orientation:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+      w: 0.0
+  twist:
+    linear:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+    angular:
+      x: 0.0
+      y: 0.0
+      z: 0.0
+  reference_frame: ''"
+		echo "CALLED MOTION COMMAND"
+		sleep 1
+		echo "SAVER STARTED"
+		rosservice call /saver/save --wait
+		sleep 1
+		echo "SAVER SAVED"
+		mv $PWD/img_tmp.png $PWD/data_$s/img_$z.png
+		echo "Moved"
+	done
+	killall image_saver
+	killall roslaunch
+	sleep 30
+done
